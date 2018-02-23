aa4265fab9648988eb0b7a9ca998d3f78b0cd027
xtenex <tenex.whatever@gmail.com>
2018-02-22 18:50:26 -0600

    Update parse.py

--- parse.py
+++ parse.py
@@ -11,25 +11,25 @@ from  collections import defaultdict
 from user_agents import parse
 
 def main():
-    r = []
-    i = defaultdict(list)
-    f = open(sys.argv[1],"r")
+    r = [] # usgin it to save data temp
+    i = defaultdict(list) # we use defaultdict to create an item array related to a key it means {key:[item1, item2, ..., itemN]}
+    f = open(sys.argv[1],"r") # We open a file from command line
     for line in f.readlines():
-        r = line.strip("\"")
-        ip = r.split(" ")[0]
-        ua = parse(filter(None, line.split("\""))[5])
+        r = line.strip("\"") #Striping first and last on each readed line
+        ip = r.split(" ")[0] #extracting IP address from readed line
+        ua = parse(filter(None, line.split("\""))[5]) # striping quotes and using parse to save user-agent
 #        print ua.browser[0] #for debug
 #        print "la ip:" + ip # for debugging prupose
 #        raw_input() # for debug
-        if i.has_key(ua.browser[0]):
-            i[ua.browser[0]].append(ip)
+        if i.has_key(ua.browser[0]): #verify if browser user-agent exist 
+            i[ua.browser[0]].append(ip) #if exist, append IP address to the browserkey
         else:
-            i[ua.browser[0]] = []
-            i[ua.browser[0]].append(ip)
+            i[ua.browser[0]] = [] #if not, we create the key on dictionary
+            i[ua.browser[0]].append(ip) # and append IP address to the item list
 
     f.close()
-    for key in i.keys():
-        print key + ":\n" + str(i[key])[1:-1]
+    for key in i.keys(): #reading dictionary key and items
+        print key + ":\n" + str(i[key])[1:-1] #Showing browser and ip´s related with that browser
 
 
 if __name__ == '__main__':
